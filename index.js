1398620e2d1483fed4763ffc7d1fb58f3b1eb40b
chiagozie pio <nduagubapio@gmail.com>
2024-07-07 11:17:22 -0700

    set up the server and connected to database

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+
+//initialize
+const app = express()
+
+const PORT = process.env.PORT || 3500
+
+// connection to database
+mongoose.connect("mongodb://0.0.0.0/airbnb-project")
+const connc = mongoose.connection
+connc.once('open',()=>{
+    console.log('connected to database');
+    app.listen(PORT, ()=>{
+        console.log(`app running on port:${PORT}`);
+    })
+});
+connc.on('error',(err)=>{
+    console.log(`database error:${err}`);
+    process.exit()
+})
